2b27ebfcf3a77fa7a8b96cc0cae30421ab96aa35
“kamesh20” <kamesh20k@gmail.com>
2022-06-26 08:22:15 +0000

    2ndcommit

--- code.java
+++ code.java
@@ -6,7 +6,7 @@ public class CurrentDateTime {
     public static void main(String[] args) {
         LocalDateTime current = LocalDateTime.now();
 
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss.SSS");
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yy-MM-dd HH:mm:ss.SSS");
         String formatted = current.format(formatter);
 
         System.out.println("Current Date and Time is: " + formatted);
